ffc837c7e3cd98f1487b88596c174947b4097d8e
RUAN FRANKLIN CASSIANO LOPES <20211014040052@ifrn.local>
2022-08-12 14:11:47 -0300

    Alterando template

--- projeto/finanças/urls.py
+++ projeto/finanças/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
+app_name='finanças'
 urlpatterns =[
     path('',views.index,name='index'),
+    #Principal view
+    path('dash/', views.PrincipalView.as_view(), name='principal'),
+    
+
+
+
 
 ]
--- projeto/finanças/views.py
+++ projeto/finanças/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,redirect, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.views import View
 from django.urls import reverse
@@ -7,7 +7,7 @@ from django.urls import reverse
 #Views da página de finanças.
 class PrincipalView(View):
     def get(self,request,*args,**kwargs):
-        return render(request,'finanças/index.html')
+        return render(request,'templates/dash.html')
 
 def index(request):
     return HttpResponse("Bem-vindo á primeira View de finanças!")
