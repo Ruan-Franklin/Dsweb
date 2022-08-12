c6864f41a595322a892bee1a3373b45a2139ef53
Ruan Franklin <ruanfranklinmengo@gmail.com>
2022-08-12 10:44:49 -0300

    Adicionando classe para o layout

--- projeto/finanças/views.py
+++ projeto/finanças/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse, Http404
+from django.views import View
+from django.urls import reverse
+
 # Create your views here.
 #Views da página de finanças.
+class PrincipalView(View):
+    def get(self,request,*args,**kwargs):
+        return render(request,'finanças/index.html')
+
 def index(request):
     return HttpResponse("Bem-vindo á primeira View de finanças!")
